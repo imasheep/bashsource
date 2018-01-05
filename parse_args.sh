62858526ca6a2cdc1a8061a76da3f80133583c14
imasheep <18622513004@163.com>
2018-01-05 23:53:26 +0800

    yangxu update parsearg script desc

--- parse_args.sh
+++ parse_args.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# 解析参数的函数 
-# 需要两个参数 
-# 1. 命令行传入的所有参数当做函数的第一个参数传入,注意引号, 比如 "-o add -p test -a main" 
-# 2. 希望解析参数后赋值的变量 "operation|product|action" 
+# 解析参数的awk函数 
+# 函数需要两个参数 
+# arg1. 命令行传入的所有参数当做函数的第一个参数传入,注意引号, 比如 "-o add -p test -a main" 
+# arg2. 希望解析参数后赋值的变量 "operation|product|action" 
 # 例 Parse_args " -o add -p test -a main"  "operation|product|action" 
 # 如按例调用函数, 则会产生operation product action三个全局变量,value分别为,add ,test, main 
 # 映射关系为首字母对应,如operation会对应 -o 后面的value, 注意变量首字母不要重复
